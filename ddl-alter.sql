d9fc4c60d29c1f4a744ab3acd75ecfbc6db9a1e2
fred-storm <fredstorm@icloud.com>
2026-02-02 22:37:01 -0600

    Adding file to showcase create a table with ddl; and one file with lots of alter examples.

---
+++ ddl-alter.sql
@@ -0,0 +1,32 @@
+--Update Producer Table to include new columns in Movies Database
+
+Use Movies
+Go
+
+Alter Table Producer
+	Add 
+	FavoriteColor VarChar(25);
+	City VarChar(40),
+	Licensed Bit,
+	StartDate Date;
+
+-- Remove FavoriteColor column from Producer Table in Movies Database
+Use Movies
+Go
+
+Alter Table Producer
+	Drop Column FavoriteColor
+
+-- Update City column to allow NULL values in Producer Table in Movies Database
+Use Movies
+Go
+
+Alter Table Producer
+	Alter Column City varchar(50) Null;
+
+-- Update Licensed column to NOT NULL in Producer Table in Movies Database
+Use Movies
+Go
+
+Alter Table Producer
+Alter Column Licensed bit not null;
